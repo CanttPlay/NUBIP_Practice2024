cf32a56c8d0130ecc59181e85943462dde9b7379
Warmoder <79647206+CanttPlay@users.noreply.github.com>
2024-07-02 18:56:16 +0300

    Ex.4
    Divisors

--- main.c
+++ main.c
@@ -1,37 +1,19 @@
 #include <stdio.h>
 
 int main() {
-    int p;
-    printf("Enter the number of digits p: ");
-    scanf("%d", &p);
+    int n;
+    printf("Enter a natural number n: ");
+    scanf("%d", &n);
 
-    if (p == 1) {
-        printf("Number of numbers: 2\n");
-        return 0;
+    int count = 0;
+
+    for (int m = 1; m < n; m++) {
+        if (n % (m + 1) == 0) {
+            count++;
+        }
     }
 
-    if (p == 2) {
-        printf("Number of numbers: 4\n");
-        return 0;
-    }
-
-    long long dp5[31] = {0};
-    long long dp9[31] = {0};
-    long long dp[31] = {0};
-
-    dp5[1] = 1;
-    dp9[1] = 1;
-    dp5[2] = 1 + 1;
-    dp9[2] = 1 + 1;
-
-    for (int i = 3; i <= p; i++) {
-        dp5[i] = dp9[i-1] + dp9[i-2];
-        dp9[i] = dp5[i-1] + dp5[i-2];
-    }
-
-    dp[p] = dp5[p] + dp9[p];
-
-    printf("Number of numbers: %lld\n", dp[p]);
+    printf("Number of equal divisors of %d: %d\n", n, count);
 
     return 0;
 }
